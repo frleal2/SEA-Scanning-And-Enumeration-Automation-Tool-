99a81ee2289b47dc0d868b6820c6514cea7ff80d
Sean Ryan Aguilar <55062065+seanraguilar@users.noreply.github.com>
2021-02-22 20:33:46 -0700

    Update DetailedViewOfSelectedTool.py

--- DetailedViewOfSelectedTool.py
+++ DetailedViewOfSelectedTool.py
@@ -8,33 +8,33 @@ class Ui_Dialog(object):
         self.groupBox_2 = QtWidgets.QGroupBox(Dialog)
         self.groupBox_2.setGeometry(QtCore.QRect(20, 40, 281, 321))
         self.groupBox_2.setObjectName("groupBox_2")
-        self.textEdit = QtWidgets.QTextEdit(self.groupBox_2)
-        self.textEdit.setGeometry(QtCore.QRect(10, 40, 251, 21))
-        self.textEdit.setObjectName("textEdit")
-        self.textEdit_2 = QtWidgets.QTextEdit(self.groupBox_2)
-        self.textEdit_2.setGeometry(QtCore.QRect(10, 80, 251, 21))
-        self.textEdit_2.setObjectName("textEdit_2")
-        self.textEdit_3 = QtWidgets.QTextEdit(self.groupBox_2)
-        self.textEdit_3.setGeometry(QtCore.QRect(10, 160, 251, 21))
-        self.textEdit_3.setObjectName("textEdit_3")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.groupBox_2)
+        self.plainTextEdit.setGeometry(QtCore.QRect(10, 40, 251, 21))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(self.groupBox_2)
+        self.plainTextEdit_2.setGeometry(QtCore.QRect(10, 80, 251, 21))
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
         self.pushButton_3 = QtWidgets.QPushButton(self.groupBox_2)
         self.pushButton_3.setGeometry(QtCore.QRect(10, 120, 61, 21))
         self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.groupBox_2)
-        self.pushButton_4.setGeometry(QtCore.QRect(200, 160, 61, 21))
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.textEdit_5 = QtWidgets.QTextEdit(self.groupBox_2)
-        self.textEdit_5.setGeometry(QtCore.QRect(10, 240, 251, 21))
-        self.textEdit_5.setObjectName("textEdit_5")
-        self.textEdit_4 = QtWidgets.QTextEdit(self.groupBox_2)
-        self.textEdit_4.setGeometry(QtCore.QRect(10, 200, 251, 21))
-        self.textEdit_4.setObjectName("textEdit_4")
+        self.plainTextEdit_5 = QtWidgets.QPlainTextEdit(self.groupBox_2)
+        self.plainTextEdit_5.setGeometry(QtCore.QRect(120, 240, 21, 21))
+        self.plainTextEdit_5.setObjectName("plainTextEdit_5")
         self.pushButton_5 = QtWidgets.QPushButton(self.groupBox_2)
         self.pushButton_5.setGeometry(QtCore.QRect(10, 280, 251, 21))
         self.pushButton_5.setObjectName("pushButton_5")
+        self.plainTextEdit_4 = QtWidgets.QPlainTextEdit(self.groupBox_2)
+        self.plainTextEdit_4.setGeometry(QtCore.QRect(10, 200, 251, 21))
+        self.plainTextEdit_4.setObjectName("plainTextEdit_4")
         self.pushButton_6 = QtWidgets.QPushButton(self.groupBox_2)
         self.pushButton_6.setGeometry(QtCore.QRect(200, 200, 61, 21))
         self.pushButton_6.setObjectName("pushButton_6")
+        self.plainTextEdit_3 = QtWidgets.QPlainTextEdit(self.groupBox_2)
+        self.plainTextEdit_3.setGeometry(QtCore.QRect(10, 160, 251, 21))
+        self.plainTextEdit_3.setObjectName("plainTextEdit_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.groupBox_2)
+        self.pushButton_4.setGeometry(QtCore.QRect(200, 160, 61, 21))
+        self.pushButton_4.setObjectName("pushButton_4")
         self.groupBox_4 = QtWidgets.QGroupBox(Dialog)
         self.groupBox_4.setGeometry(QtCore.QRect(320, 40, 261, 351))
         self.groupBox_4.setObjectName("groupBox_4")
@@ -42,7 +42,7 @@ class Ui_Dialog(object):
         self.pushButton.setGeometry(QtCore.QRect(60, 430, 56, 17))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(170, 430, 56, 17))
+        self.pushButton_2.setGeometry(QtCore.QRect(130, 430, 56, 17))
         self.pushButton_2.setObjectName("pushButton_2")
 
         self.retranslateUi(Dialog)
@@ -52,35 +52,15 @@ class Ui_Dialog(object):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.groupBox_2.setTitle(_translate("Dialog", "Tool Specification"))
-        self.textEdit.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Tool Name</p></body></html>"))
-        self.textEdit_2.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Tool Description</p></body></html>"))
-        self.textEdit_3.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Option and Argument</p></body></html>"))
+        self.plainTextEdit.setPlainText(_translate("Dialog", "Tool Name"))
+        self.plainTextEdit_2.setPlainText(_translate("Dialog", "Tool Description"))
         self.pushButton_3.setText(_translate("Dialog", "Tool Path"))
-        self.pushButton_4.setText(_translate("Dialog", "+"))
-        self.textEdit_5.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">OR</p></body></html>"))
-        self.textEdit_4.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Output Data Specification</p></body></html>"))
+        self.plainTextEdit_5.setPlainText(_translate("Dialog", "OR"))
         self.pushButton_5.setText(_translate("Dialog", "Tool Specification File"))
+        self.plainTextEdit_4.setPlainText(_translate("Dialog", "Output Data Specification"))
         self.pushButton_6.setText(_translate("Dialog", "+"))
+        self.plainTextEdit_3.setPlainText(_translate("Dialog", "Option and Argument"))
+        self.pushButton_4.setText(_translate("Dialog", "+"))
         self.groupBox_4.setTitle(_translate("Dialog", "Tool Dependency"))
         self.pushButton.setText(_translate("Dialog", "Save"))
         self.pushButton_2.setText(_translate("Dialog", "Cancel"))
