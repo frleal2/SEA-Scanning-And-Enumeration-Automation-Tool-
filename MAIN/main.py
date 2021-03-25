2eae67978a18e62f3325cd683ee966e6c7064781
ahaguilar <ahaguilar@miners.utep.edu>
2021-03-25 00:17:03 -0600

    corrected, main works for browse button

--- MAIN/main.py
+++ MAIN/main.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui 
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from DB.db import databaseHandler
 import pymongo
 import pandas as pd
-import sys 
+import sys
 
 # GUI FILE
 from SEA_main_page import Ui_MainWindow
@@ -16,14 +16,14 @@ db.build()
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        #DEFAULT PAGE 
+        #DEFAULT PAGE
         self.ui.stackedWidget.setCurrentWidget(self.ui.tool)
 
-        # TOOL PAGE 
+        # TOOL PAGE
         self.ui.btn_tool.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.tool))
 
         # RUN PAGE
@@ -38,6 +38,10 @@ class MyWindow(QMainWindow):
         #SAVE TOOL INPUT INTO TOOL TABLE AT DATABASE
         self.ui.save_tool_button.clicked.connect(self.saveToolInput)
         self.ui.save_tool_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.tool))
+
+        self.ui.browse_path_button_3.clicked.connect(self.open)
+
+        self.ui.browse_path_button.clicked.connect(self.open1)
 
         #TESTING PANDAS
         #db.Tool.find()
@@ -73,10 +77,20 @@ class MyWindow(QMainWindow):
 
         db.insertIntoTool(toolName, toolDesc, toolPath, toolOutputDataSpec, optionArg)
 
-    
-        
-        
-        
+    def open(self):
+        path=QFileDialog.getOpenFileName(self,r'Open a file',r'/home/hiram/Documents/Tool_Specification/',r'All files(*.*)')
+        if path != ('',''):
+            print("File path:"+path[0])
+
+    def open1(self):
+        path=QFileDialog.getOpenFileName(self,r'Open a file',r'/usr/bin/nmap',r'All files(*.*)')
+        if path != ('',''):
+            print("File path:"+path[0])
+
+
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
